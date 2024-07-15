2993aca8f9438dfbda15c7325824546407c6cd13
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-07-15 18:05:19 +0530

    486A - Calculating Function

---
+++ 486A - Calculating Function/486A - Calculating Function.cpp
@@ -0,0 +1,19 @@
+#include iostream
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin  n;
+    
+    long long result;
+    if (n % 2 == 0) {
+        result = n  2;
+    } else {
+        result = -(n + 1)  2;
+    }
+    
+    cout  result  endl;
+    
+    return 0;
+}
