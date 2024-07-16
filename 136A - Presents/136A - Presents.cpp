162a6808d1d4fa26d3320ee912e910eac355dbb6
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-07-16 10:49:19 +0530

    Create 136A - Presents.cpp

---
+++ 136A - Presents/136A - Presents.cpp
@@ -0,0 +1,29 @@
+Sure, here's the C++ code without comments:
+
+```cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> p(n);
+    vector<int> result(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> p[i];
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        result[p[i] - 1] = i + 1;
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        cout << result[i] << " ";
+    }
+    
+    return 0;
+}
+```
