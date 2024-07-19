facc420ae596d6355ad1fad67ea8f818c360b8fd
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-07-19 08:14:20 +0530

    Update 61A - Ultra-Fast Mathematician.cpp

--- 61A - Ultra-Fast Mathematician/61A - Ultra-Fast Mathematician.cpp
+++ 61A - Ultra-Fast Mathematician/61A - Ultra-Fast Mathematician.cpp
@@ -6,15 +6,11 @@ using namespace std;
 int main() {
     string num1, num2;
     
-    // Read the two binary numbers
     cin >> num1 >> num2;
     
-    // Initialize the result string
     string result = "";
     
-    // Iterate over the length of the numbers
     for (int i = 0; i < num1.length(); ++i) {
-        // Compare the corresponding characters
         if (num1[i] != num2[i]) {
             result += '1';
         } else {
@@ -22,7 +18,6 @@ int main() {
         }
     }
     
-    // Output the result
     cout << result << endl;
     
     return 0;
