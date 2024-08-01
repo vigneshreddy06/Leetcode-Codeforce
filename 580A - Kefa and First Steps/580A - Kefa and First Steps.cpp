5c35e8cfa61e424dda8c257f07edd7f0dac79aee
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-08-02 01:39:48 +0530

    Create 580A - Kefa and First Steps.cpp

---
+++ 580A - Kefa and First Steps/580A - Kefa and First Steps.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int maxLength = 1;
+    int currentLength = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] >= a[i - 1]) {
+            currentLength++;
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+            }
+        } else {
+            currentLength = 1;
+        }
+    }
+
+    cout << maxLength << endl;
+    return 0;
+}
