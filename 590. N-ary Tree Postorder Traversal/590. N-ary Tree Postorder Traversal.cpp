117516502df00fcc41d57c71060de0855e1036b2
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-08-26 18:13:10 +0530

    Update 590. N-ary Tree Postorder Traversal.cpp

--- 590. N-ary Tree Postorder Traversal/590. N-ary Tree Postorder Traversal.cpp
+++ 590. N-ary Tree Postorder Traversal/590. N-ary Tree Postorder Traversal.cpp
@@ -1,16 +1,17 @@
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public List<Node> children;
+public:
+    int val;
+    vector<Node*> children;
 
-    public Node() {}
+    Node() {}
 
-    public Node(int _val) {
+    Node(int _val) {
         val = _val;
     }
 
-    public Node(int _val, List<Node> _children) {
+    Node(int _val, vector<Node*> _children) {
         val = _val;
         children = _children;
     }
@@ -18,24 +19,25 @@ class Node {
 */
 
 class Solution {
-    public List<Integer> postorder(Node root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        if(root==null){
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> list;
+        if(root==NULL){
             return list; 
         }
         help(list,root);
-        list.add(root.val);
+        list.push_back(root->val);
         return list;
     }
-    public void help(ArrayList<Integer> list,Node root){
-        List<Node> li = new ArrayList<>();
-        li = root.children;
+    void help(vector<int>& list,Node* root){
+        vector<Node*> li;
+        li = root->children;
         for(int i=0;i<li.size();i++){
-            if(li.get(i)==null){
+            if(li[i]==NULL){
                 continue;
             }
-            help(list,li.get(i));
-            list.add(li.get(i).val);
+            help(list,li[i]);
+            list.push_back(li[i]->val);
         }
     }
-}
+};
