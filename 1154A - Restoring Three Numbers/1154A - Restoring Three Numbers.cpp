9b910fc6625a8527b35f69fd8eae3adc8ed1d31a
Vigneshreddy06 <vigneshreddy065@gmail.com>
2024-09-05 18:34:34 +0530

    Update 1154A - Restoring Three Numbers.cpp

---
+++ 1154A - Restoring Three Numbers/1154A - Restoring Three Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int x[4];
+    for (int i = 0; i < 4; i++) cin >> x[i];
+    sort(x, x + 4);
+    int a = x[3] - x[0];
+    int b = x[3] - x[1];
+    int c = x[3] - x[2];
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
